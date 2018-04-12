81e719c6edd70f1be931425ffd77abfc805b3f9f
Mikaal Naik <matthewnaik@gmail.com>
2018-04-12 15:17:19 +0000

    Initial commit

---
+++ whereswaldo.js
@@ -0,0 +1,40 @@
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(arr, i) {
+    if (arr === "Waldo") {
+      found(i);   // execute callback
+    }
+  })
+}
+function actionWhenFound(i) {
+  console.log("Found him at index " + i);
+}
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+//
+// function actionWhenFound(i) {
+//   console.log("Found him at index " + i);
+// }
+//
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
